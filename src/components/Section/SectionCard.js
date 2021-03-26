38bcbf35751fa51eeb6ad747300b5be5de341e9d
Navkcalbeht32 <asumoski@gmail.com>
2021-03-26 16:59:48 -0400

    hero text, portfolio anim, animations, boxshadow, section cards update

--- src/components/Section/SectionCard.js
+++ src/components/Section/SectionCard.js
@@ -2,9 +2,11 @@ import './Section.scss'
 const SectionCard = ({children, title, text}) => {
     return (
         <div className="section-card">
-            <h3>{title}</h3>
-            <h5 className="section-card-text">{text}</h5>
-            {children}
+            <div className="section-card-wrapper">
+                <h3>{title}</h3>
+                <h5 className="section-card-text">{text}</h5>
+                {children}
+            </div>
         </div>
     )
 }
